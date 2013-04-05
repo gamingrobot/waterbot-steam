e3d0a35544ca4b3592e66950a07ffcf8cf6b886c
GamingRobot <gamingrobot@gmail.com>
2013-04-04 23:26:34 -0600

    added auto updator

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import subprocess
-import os
+import shlex
 import time
 try:
     from Queue import Queue, Empty
@@ -20,7 +20,43 @@ class WatchDog:
         self._watcher()
 
     def _restartBot(self):
+        self._updateBot()
         self._openBot()
+
+    def _exec_shell(command):
+        """Execute a shell command and get the output."""
+        command = shlex.split(command)
+        result = subprocess.check_output(command, stderr=subprocess.STDOUT)
+        if result:
+            result = result[:-1]  # strip newline if command returned anything
+        return result
+
+    def _updateBot(self):
+        try:
+            dry_fetch = self._exec_shell("git fetch --dry-run")
+        except subprocess.CalledProcessError:
+            print "Cannot fetch; do you have a remote repository configured correctly?"
+            return
+        try:
+            last_commit = self._exec_shell("git log -n 1 --pretty=\"%ar\"")
+        except subprocess.CalledProcessError:
+            last_commit = "never"  # couldn't get a log, so no commits
+
+        if not dry_fetch:  # no new changes to pull
+            print "No new changes. Last commit was %s." % last_commit
+
+        else:  # stuff has happened!
+            print "There are new changes upstream..."
+            status = self._exec_shell("git status")
+
+            if status.endswith("nothing to commit (working directory clean)"):
+                print "Pulling new changes..."
+                result = self._exec_shell("git pull")
+                print "The following changes have been made since %s:" % last_commit
+                print result
+            else:
+                print "Warning: you have uncommitted changes in this repository!"
+                print "Ignoring."
 
     def _openBot(self):
         self.bot = subprocess.Popen(["ipy", '-X:Frames', '-u', 'bot.py', config], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, close_fds=ON_POSIX)
