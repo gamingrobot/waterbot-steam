57528c071e734a8296bf482e666110a54ca38c98
GamingRobot <gamingrobot@gmail.com>
2013-04-05 00:19:37 -0600

    removed old code

--- main.py
+++ main.py
@@ -25,27 +25,9 @@ class WatchDog:
         self._openBot()
 
     def _updateBot(self):
-        """dry_fetch = self._exec_shell("git fetch")
-        last_commit = self._exec_shell("git log -n 1 --pretty=\"%ar\"")
-        print "dry fetch" + dry_fetch
-        if dry_fetch != "":  # new changes
-            print "There are new changes upstream..."
-            status = self._exec_shell("git status")
-
-            if status.endswith("nothing to commit (working directory clean)"):
-                print "Pulling new changes..."
-                result = self._exec_shell("git pull")
-                print "The following changes have been made since %s:" % last_commit
-                print result
-            else:
-                print "Warning: you have uncommitted changes in this repository!"
-                print "Ignoring."
-
-        else:  # no new changes
-            print "No new changes. Last commit was %s." % last_commit"""
-
         g = git.cmd.Git(os.getcwd())
-        g.pull()
+        print g.fetch()
+        print g.pull()
 
     def _openBot(self):
         self.bot = subprocess.Popen(["ipy", '-X:Frames', '-u', 'bot.py', config], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, close_fds=ON_POSIX)
