d8f071fe01b876d1f362fcb36a28abbd5f835e18
GamingRobot <gamingrobot@gmail.com>
2013-04-04 23:32:21 -0600

    update test

--- plugins/base/base.py
+++ plugins/base/base.py
@@ -21,7 +21,7 @@ class Base:
     def restartBot(self, command, args):
         t = Timer(1, manager.restartBot)
         t.start()
-        return "brb restart", True
+        return "brb restarting", True
 
     def echoCommand(self, command, args):
         return " ".join(args), args
