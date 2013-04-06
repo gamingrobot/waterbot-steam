3423c689e5260bd9406fe4d98346d87a8801ead4
GamingRobot <gamingrobot@gmail.com>
2013-04-06 03:54:21 -0600

    removed test

--- plugins/markov/markov.py
+++ plugins/markov/markov.py
@@ -4,6 +4,7 @@ import clr
 class Markov:
     def __init__(self, xml):
         manager.commandmanager.registerCommand("mk", self.markovCommand)
+        raise
 
     def markovCommand(self, command, args, source):
         return "hi"
